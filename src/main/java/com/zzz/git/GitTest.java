015cffbe47de049890659e8b8a7c9843bd25a825
zzz <648949599@qq.com>
2022-11-03 15:03:27 +0800

    first commit

---
+++ src/main/java/com/zzz/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zzz.git;
+
+/**
+ * @Description：
+ * @author： zzz
+ * @date 2022年11月03日 15:01
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
