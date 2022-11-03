19ab2e3c63cd473d62411421cb89fbf8fbeba86b
zzz <648949599@qq.com>
2022-11-03 15:11:27 +0800

    hot-fix test commit

--- src/main/java/com/zzz/git/GitTest.java
+++ src/main/java/com/zzz/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello gitHub!");
         System.out.println("hello gitHub2!");
         System.out.println("hello gitHub3!");
+        System.out.println("hot-fix test");
     }
 }
